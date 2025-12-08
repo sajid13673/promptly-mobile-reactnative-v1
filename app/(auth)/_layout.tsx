27550500da3be04ce4d43ea4816de749063b4f12
Sajid Ahamed <sajid13673@gmail.com>
2025-12-08 16:49:38 +0530

    feat(auth): add tab layout for login and register screens with vector icons support

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,11 +1,54 @@
-import React from 'react'
-import { View } from 'react-native'
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import React from "react";
 
 const _layout = () => {
   return (
-    <View>
-    </View>
-  )
-}
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: 'center',
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0f0D23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: "absolute",
+          overflow: 'hidden',
+          borderWidth: 1,
+          borderColor: "#0f0D23",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="login"
+        options={{
+          title: "Login",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="log-in-outline" size={size} color={color} />
+          ),
+        }}
+      />
 
-export default _layout
+      <Tabs.Screen
+        name="register"
+        options={{
+          title: "Register",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-add-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default _layout;

904a286ffe392b31baa4f97b0f0380c0e361c3c8
Sajid Ahamed <sajid13673@gmail.com>
2026-01-06 19:28:44 +0530

    feat: hide headers for auth and app stacks

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,5 +2,10 @@ import { Stack } from "expo-router";
 import "./global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(app)" options={{ headerShown: false }} />
+    </Stack>
+  );
 }

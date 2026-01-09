e27a3f33263a73fece6a17fd576d8f7fb53ea1ec
Sajid Ahamed <sajid13673@gmail.com>
2026-01-09 23:38:12 +0530

    feat: add UI contents to home screen

--- app/(app)/home.tsx
+++ app/(app)/home.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { MaterialIcons } from "@expo/vector-icons";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
-const home = () => {
+export default function Home() {
   return (
-    <View>
-      <Text>home</Text>
-    </View>
-  )
-}
+    <View className="flex-1 bg-primary p-4 justify-between">
+      <View className="flex-1 justify-center">
+        <Text className="text-white text-2xl font-semibold text-center">
+          What's on your mind today?
+        </Text>
+      </View>
 
-export default home
+      <View className="border-gray-500 border-2 rounded-3xl flex-row items-center py-3 px-3 mb-4 gap-2">
+        <TextInput
+          placeholder="Ask me anything"
+          placeholderTextColor="#ADADAD"
+          className="flex-1 text-white pr-2"
+        />
+        <TouchableOpacity>
+          <MaterialIcons name="send" size={24} color="white" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}

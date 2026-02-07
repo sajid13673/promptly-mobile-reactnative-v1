b3a1393ec9f5b69977196c94d1aaad1d55ee35e0
Sajid Ahamed <sajid13673@gmail.com>
2026-02-08 01:53:12 +0530

    refactor(auth): redirect unauthenticated users to login

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,6 +1,11 @@
-import { Stack } from "expo-router";
+import { useAuth } from "@/context/AuthContext";
+import { Redirect, Stack } from "expo-router";
 
-export default function RootLayout() {
+export default function AppLayout() {
+  const { user, loading } = useAuth();
+  if (!user) {
+    return <Redirect href="/(auth)/login" />;
+  }
   return (
     <Stack>
       <Stack.Screen name="home" options={{ headerShown: false }} />

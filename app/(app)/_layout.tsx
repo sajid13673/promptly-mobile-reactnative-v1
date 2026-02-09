0d3e3be6d2625dfc907f3460e15d4388e68a34d9
Sajid Ahamed <sajid13673@gmail.com>
2026-02-10 01:56:35 +0530

    refactor(navigation): migrate from stack to drawer navigation for main app flow

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,14 +1,37 @@
 import { useAuth } from "@/context/AuthContext";
-import { Redirect, Stack } from "expo-router";
+import { Redirect } from "expo-router";
+import { Drawer } from "expo-router/drawer";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function AppLayout() {
   const { user, loading } = useAuth();
+
+  if (loading) return null;
   if (!user) {
     return <Redirect href="/(auth)/login" />;
   }
+
   return (
-    <Stack>
-      <Stack.Screen name="home" options={{ headerShown: false }} />
-    </Stack>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer
+        screenOptions={{
+          headerShown: true,
+          headerStatusBarHeight: 1,
+          drawerContentStyle: {
+            backgroundColor: "#151312",
+          },
+          headerTintColor: "#fff",
+          headerTransparent: true,
+        }}
+      >
+        <Drawer.Screen
+          name="home"
+          options={{
+            drawerLabel: "Home",
+            title: "",
+          }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
   );
 }
